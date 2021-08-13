37cf000a581a35eaa7470f8fd3561c0bd39808d5
Melanie Melkonyan <milli3377mel@gmail.com>
2021-08-13 17:56:54 +0400

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <p>
+      adsads
+      </p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
